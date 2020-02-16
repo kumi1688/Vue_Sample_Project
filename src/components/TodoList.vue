b3405ae8ee1548b943847c2903ed597130dc2df5
Ubuntu <ubuntu@ip-172-31-43-67.ap-northeast-2.compute.internal>
2020-02-16 13:44:55 +0000

    applying new function for better UX

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,14 +1,14 @@
 <template>
     <section>
-        <ul>
-            <li v-for="(todoItem, index) in propsdata" :key='index' class='shadow'> 
+        <transition-group name='list' tag='ul'>
+            <li v-for="(todoItem, index) in propsdata" :key='todoItem' class='shadow'> 
                 <i class='checkBtn fas fa-check' aria-hidden='true'></i>
                     {{ todoItem }}
                 <span class="removeBtn" type='botton' @click='removeTodo(todoItem, index)'>
                     <i class='far fa-trash-alt' aria-hidden="true"></i>
                 </span>
             </li>
-        </ul>
+        </transition-group>
     </section>
 </template>
 
@@ -41,15 +41,20 @@
         background: white;
         border-radius: 5px;
     }
-
     .checkBtn{
         line-height: 45px;
         color: #62acde;
         margin-right: 5px;
     }
-
     .removeBtn{
         margin-left: auto;
         color: #de4343;
+    }
+    .list-enter-active, .list-leave-active{
+        transition: all 1s;
+    }
+    .list-enter, .list-leave-to {
+        opacity: 0;
+        transform: translateY(30px);
     }
 </style>
