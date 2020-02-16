11ed5a19fe3ff4d39a3a97c0f94c58235c6740f7
Ubuntu <ubuntu@ip-172-31-43-67.ap-northeast-2.compute.internal>
2020-02-16 12:53:13 +0000

    [06-4] building component TodoInput, TodoList, TodoHeader, TodoFooter

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -1,6 +1,10 @@
 <template>
     <div>
-        Todo Header
+        <header>
+            <h1>
+            Todo Header
+        </h1>
+        </header>
     </div>
 </template>
 
@@ -11,5 +15,9 @@
 </script>
 
 <style scoped>
-
+    h1{
+        color : #2f3b52;
+        font-weight: 900;
+        margin: 2.5rem 0 1.5rem;
+    }
 </style>
--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,15 +1,58 @@
 <template>
-    <div>
-        TodoInput
+    <div class='inputBox shadow'>
+        <input v-model="newTodoItem" type="text" placeholder='Type What you have to do'
+        @keyup.enter="addTodo">
+        <span class="addContainer" @click="addTodo">
+            <i class='addBtn fas fa-plus' aria-hidden='true'></i>
+        </span>
     </div>
 </template>
 
 <script>
     export default {
-        
+        data() {
+            return{
+                newTodoItem: ''
+            }
+        },
+        methods: {
+            addTodo() {
+                if(this.newTodoItem !== ''){
+                    var value = this.newTodoItem && this.newTodoItem.trim();
+                    localStorage.setItem(value, value);
+                    this.clearInput();
+                }
+            },
+            clearInput() {
+                this.newTodoItem = '';
+            }
+        }
     }
 </script>
 
 <style scoped>
-
+    input:focus{
+        outline: none;
+    }
+    .inputBox{
+        background: white;
+        height: 50px;
+        line-height: 50px;
+        border-radius: 5px;
+    }
+    .inputBox input{
+        border-style: none;
+        font-size: 0.9rem;
+    }
+    .addContainer{
+        float: right;
+        background: linear-gradient(to right, #6478fb, #8763fb);
+        display: block;
+        width: 3rem;
+        border-radius: 0 5px 5px 0;
+    }
+    .addBtn{
+        color: white;
+        vertical-align: middle;
+    }
 </style>
